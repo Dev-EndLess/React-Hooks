ce66bebefd063e3caf9dace92555b7303d40badb
Dev-EndLess <endlesspain1988@gmail.com>
2023-02-06 10:33:19 +0100

    dave gray example upload

---
+++ useCallback/src/App1.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+ export const App1 = () => {
+  const [userInput, setUserInput] = useState('');
+  const [result, setResult] = useState(0);
+  const [num1] = useState(4);
+  const [num2] = useState(5);
+
+  //const sum = useCallback(() => num1 + num2, [num1, num2]);
+
+  const buildArray = useCallback(() => [num1, num2], [num1, num2]);
+
+  // useEffect(() => {
+  //   console.log(`New Sum Value: ${sum()}`);
+  //   setResult(sum());
+  // }, [sum]);
+
+  useEffect(() => {
+    console.log(`New array: ${buildArray()}`);
+    setResult(buildArray());
+  }, [buildArray]);
+
+  return (
+    <main className="App">
+      <input type="text" placeholder="input" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+      <h1>Output: {userInput || "--"}</h1>
+      <p>Result: {JSON.stringify(result)}</p>
+    </main>
+  )
+
+}
